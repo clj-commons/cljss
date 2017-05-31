ba795075cb722573dd349ce1f2adbd14462b8a11
Roman Liutikov <roman01la@romanliutikov.com>
2017-06-01 01:42:46 +0300

    do not gen var cls if no vars

--- src/cljss/core.cljs
+++ src/cljss/core.cljs
@@ -5,6 +5,8 @@
 (defonce ^:private sheet (create-sheet))
 
 (defn css [cls vars]
-  (let [var-cls (str "vars-" cls)]
-    (insert! sheet (build-css var-cls vars))
-    (str "css-" cls " " var-cls)))
+  (if (seq vars)
+    (let [var-cls (str "vars-" cls)]
+      (insert! sheet (build-css var-cls vars))
+      (str "css-" cls " " var-cls))
+    (str "css-" cls)))
