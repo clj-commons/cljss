49a559e4213d334ae8890a03270adeddb3b70616
Roman Liutikov <roman01la@romanliutikov.com>
2017-06-01 03:26:37 +0300

    gen diff var class

--- src/cljss/core.cljs
+++ src/cljss/core.cljs
@@ -6,7 +6,7 @@
 
 (defn css [cls vars]
   (if (seq vars)
-    (let [var-cls (str "vars-" cls)]
+    (let [var-cls (name (gensym "vars-"))]
       (insert! sheet (build-css var-cls vars))
       (str "css-" cls " " var-cls))
     (str "css-" cls)))
