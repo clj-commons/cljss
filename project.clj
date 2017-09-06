c8616e7cf03018b814e0141648885aed62cd9dc9
Roman Liutikov <roman01la@romanliutikov.com>
2017-09-06 20:12:26 +0300

    v1.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.roman01la/cljss "1.3.1"
+(defproject org.roman01la/cljss "1.4.0"
   :description "Clojure Style Sheets"
   :url "https://github.com/roman01la/cljss"
   :license {:name "Eclipse Public License"
