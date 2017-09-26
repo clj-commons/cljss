d21155f0010acd4a42fabf8bd6b242f81a684946
Roman Liutikov <roman01la@romanliutikov.com>
2017-09-26 13:10:52 +0300

    fix tests

--- test/cljss/media_test.clj
+++ test/cljss/media_test.clj
@@ -4,23 +4,23 @@
 
 (deftest test-media-queries
   (testing "simple media query"
-    (is (render-media-expr {:print true})
-        "print"))
+    (is (= (render-media-expr {:print true})
+           "print")))
   (testing "simple media query with not"
-    (is (render-media-expr {:print false})
-        "not print"))
+    (is (= (render-media-expr {:print false})
+           "not print")))
   (testing "comma separated media query"
-    (is (render-media-expr [{:print true} {:screen false}])
-        "print, not screen"))
+    (is (= (render-media-expr [{:print true} {:screen false}])
+           "print, not screen")))
   (testing "comma separated media query"
-    (is (render-media-expr {:max-width "768px"})
-        "(max-width: 768px)"))
+    (is (= (render-media-expr {:max-width "768px"})
+           "(max-width: 768px)")))
   (testing "compound media query"
-    (is (render-media-expr {:speech true :aspect-ratio "11/5"})
-        "speech and (aspect-ratio: 11/5)"))
+    (is (= (render-media-expr {:speech true :aspect-ratio "11/5"})
+           "speech and (aspect-ratio: 11/5)")))
   (testing "media feature without value"
-    (is (render-media-expr {:color nil})
-        "(color)"))
+    (is (= (render-media-expr {:color nil})
+           "(color)")))
   (testing "media feature with :only"
-    (is (render-media-expr {:screen :only})
-        "only screen")))
+    (is (= (render-media-expr {:screen :only})
+           "only screen"))))
