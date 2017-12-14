275c43f3536f771d8e243a63b7b76c2d78311d43
Roman Liutikov <roman01la@romanliutikov.com>
2017-12-14 21:32:27 +0200

    bump deps

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-beta3" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.946" :scope "provided"]]
 
   :test-paths ["test/clj"]
