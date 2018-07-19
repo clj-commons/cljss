1fcb78e9ef02068e2a0007c86acd5a2382f244ab
Roman Liutikov <roman01la@romanliutikov.com>
2018-07-19 14:15:57 +0300

    update tests

--- test/clj/cljss/core_test.clj
+++ test/clj/cljss/core_test.clj
@@ -1,78 +1,31 @@
 (ns cljss.core-test
   (:require [clojure.test :refer :all]
             [cljss.core :refer :all]
+            [cljss.rum :refer :all]
             [cljss.builder :refer :all]
             [cljss.font-face :as ff]
             [cljss.inject-global :as ig]))
 
-(def basic-styles {:color "red"})
+(deftest test-styles-builder
+  (testing "static"
+    (is (= (build-styles "cls" {:color "#fff"})
+           ["cls" [".cls{color:#fff;}"] []])))
 
-(def dynamic-styles {:background-color #(:bg %)
-                     :margin           #(if (:large %) "10px" "5px")})
+  (testing "pseudos"
+    (is (= (build-styles "cls" {:&:hover {:color "red"}})
+           ["cls" [".cls{}" ".cls:hover{color:red;}"] []])))
 
-(def pseudo-styles (merge basic-styles {:&:hover {:color "blue"}}))
+  (testing "string selector"
+    (is (= (build-styles "cls" {"a" {:color "blue"}})
+           ["cls" [".cls{}" ".cls a{color:blue;}"] []])))
 
-(def pseudo-dynamic-styles
-  (assoc
-    pseudo-styles
-    :&:active
-    {:color #(:active-color %)}))
+  (testing "dynamic"
+    (is (= (build-styles "cls" {:color 'x})
+           '["cls" [".cls{color:var(--var-cls-0);}"] [["--var-cls-0" x]]])))
 
-(def complete-styles
-  (merge
-    basic-styles
-    dynamic-styles
-    pseudo-dynamic-styles))
-
-;;; unique ids for style classes are generated
-;;; from the hashed set of non pseudo styles
-;;; this function replicates initialization
-;;; of styles in cljss.builder/build-styles
-;;; and produces the same hashed value used in
-;;; style ids
-(defn remove-pseudo [styles]
-  (filterv
-    (comp not #'cljss.builder/pseudo?)
-    styles))
-
-(deftest test-build-styles
-  (testing "building basic styles"
-    (let [[[id static vals]] (build-styles "test" basic-styles)]
-      (is (= "test" id))
-      (is (= ".test{color:red;}" static))
-      (is (empty? vals))))
-
-  (testing "building dynamic styles"
-    (let [[[id static vals]] (build-styles "test" dynamic-styles)]
-      (is (= "test" id))
-      (is (= ".test{background-color:var(--var-test-0);margin:var(--var-test-1);}" static))
-      (is (= [["--var-test-0" (:background-color dynamic-styles)] ["--var-test-1" (:margin dynamic-styles)]] vals))))
-
-  (testing "building basic pseudo styles"
-    (let [result (build-styles "test" pseudo-styles)]
-      (is (= result
-             [["test" ".test{color:red;}" []]
-              [".test:hover" ".test:hover{color:blue;}" []]]))))
-
-  (testing "building dynamic pseudo styles"
-    (let [result (build-styles "test" pseudo-dynamic-styles)]
-      (is (= result
-             [["test"
-               ".test{color:red;}"
-               [["--var-test-1" (get-in pseudo-dynamic-styles [:&:active :color])]]]
-              [".test:hover" ".test:hover{color:blue;}" []]
-              [".test:active" ".test:active{color:var(--var-test-1);}" []]]))))
-
-  (testing "building a complete set of styles"
-    (let [result (build-styles "test" complete-styles)]
-      (is (= result
-             [["test"
-               ".test{color:red;background-color:var(--var-test-0);margin:var(--var-test-1);}"
-               [["--var-test-0" (:background-color complete-styles)]
-                ["--var-test-1" (:margin complete-styles)]
-                ["--var-test-3" (get-in complete-styles [:&:active :color])]]]
-              [".test:hover" ".test:hover{color:blue;}" []]
-              [".test:active" ".test:active{color:var(--var-test-3);}" []]])))))
+  (testing "dynamic pseudos"
+    (is (= (build-styles "cls" {:&:hover {:color 'x}})
+           '["cls" [".cls{}" ".cls:hover{color:var(--var-cls-0);}"] [["--var-cls-0" x]]]))))
 
 (deftest test-font-face
   (testing "build @font-face"
