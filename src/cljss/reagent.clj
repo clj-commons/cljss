a4b3c8716e269ba07246cf6f67d75162fc0b8b8f
Ralph Parkison <chesslunatic@gmail.com>
2021-06-04 14:00:20 -0500

    reagent.core/create-element

--- src/cljss/reagent.clj
+++ src/cljss/reagent.clj
@@ -5,7 +5,7 @@
   (let [cls-name# (var->cls-name var)
         cmp-name# (var->cmp-name var)
         [tag# static# vals# attrs#] (->styled tag styles cls-name#)
-        create-element# `#(apply js/React.createElement ~tag# (cljs.core/clj->js %1) (map reagent.core/as-element %2))]
+        create-element# `#(apply reagent.core/create-element ~tag# (cljs.core/clj->js %1) (map reagent.core/as-element %2))]
     `(do
        (def ~var
          (cljss.reagent/styled ~cls-name# ~static# ~vals# ~attrs# ~create-element#))
